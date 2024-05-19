a590321ea9f0be5399106a591544398a9197fddf
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-19 20:51:37 +0530

    Update body.jsx

--- frontend/src/component/body.jsx
+++ frontend/src/component/body.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { Day, Night, Verticle } from '../svg/svg'
+import { useTheme } from '../context/themeContext';
 const Body = () => {
+  const {allTheme} = useTheme();
   return (
-    <div className='bg-gray-300 h-[calc(100vh-3.5rem)] w-screen b'>
-      <div className='flex w-screen h-12 rounded-t-lg justify-start items-center'>
-        <Verticle />
-        <Day />
-        <Night />
-        <select name="cars" id="cars" className='border-none outline-none rounded-sm size- w-18'>
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="mercedes">Mercedes</option>
-          <option value="audi">Audi</option>
+    <div className=' h-[calc(100vh-3.5rem)] w-screen b'>
+      <div className='h-11 flex items-center pl-2 gap-1 border-t-2 border-b-2' style={{backgroundColor:allTheme.body,borderColor:allTheme.borderColor}}>
+      <select name="cars" id="cars" className='bg-blue-400 outline-none text-white rounded-sm h-full pl-3 pr-3'>
+          <option value="volvo">Python</option>
+          <option value="saab">JavaScript</option>
+          <option value="mercedes">C</option>
+          <option value="audi">Rust</option>
         </select>
-      </div>
-      <div className='h-[calc(100vh-6.5rem)] w-screen flex'>
-        <textarea className='bg-slate-500 w-1/2 h-[calc(100vh-6.5rem)] resize-none outline-none'>ddddd</textarea>
-        
-        <div className='bg-slate-900 w-1/2 h-[calc(100vh-6.5rem)]'></div>
-
+        <button className='bg-blue-400  rounded-sm h-full pl-6 pr-6 text-lg text-white'>Run</button>
+        </div>
+      <div className=' h-[calc(100vh-6.3rem)] w-screen flex'>
+        <textarea style={{backgroundColor:allTheme.body,color:allTheme.textColor,borderColor:allTheme.borderColor}} className='border-r-2 w-1/2 h-full resize-none outline-none'>ddddd</textarea>
+        <div className=' w-1/2  h-full' style={{backgroundColor:allTheme.body,color:allTheme.textColor}}></div>
       </div>
     </div>
   )
