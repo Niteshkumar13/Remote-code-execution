a29b0ff59a5c87cc6534d5e25605348840689a57
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-28 21:54:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkHover: "#f5f5f5",
+    },
+    },
   },
   plugins: [],
 }
-
