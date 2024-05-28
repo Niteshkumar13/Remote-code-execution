f5a27382f0fc76729f8eb924b4173151375770ef
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-28 23:23:56 +0530

    Update body.tsx

--- src/component/body.tsx
+++ src/component/body.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useTheme } from '../context/themeContext';
 import Editors from './editor';
 import {useLanguage}  from '../context/selectLanguage';
@@ -8,6 +8,7 @@ import { usechangetheme } from '../hook/changeTheme';
 import Slider from './slider';
 const Body = () => {
   const { allTheme,setAllTheme  } = useTheme();
+  const [showSlide,setShowSlide] = useState<boolean>(false)
   const {lan,setLan}  = useLanguage()
   const {change} = usechangetheme({ allTheme, setAllTheme })
   const changeLan = (e:any)=>{
@@ -16,8 +17,8 @@ const Body = () => {
   return (
     <div className=' h-[calc(100vh-3.5rem)] w-screen '>
       <div className='h-11 flex items-center pl-1 gap-[2px] border-t-2 border-b-2' style={{ backgroundColor: allTheme.body, borderColor: allTheme.borderColor }}>
-        <button className='bg-blue-400 h-full p-1 pl-2 pr-2 rounded'><LuFiles size={25}/></button>
-        <Slider/>
+        <button className='bg-blue-400 h-full p-1 pl-2 pr-2 rounded' onClick={e=>setShowSlide(!showSlide)}><LuFiles size={25}/></button>
+        {showSlide&&<Slider/>}
         <button className='bg-blue-400 h-full pl-2 pr-2 rounded' onClick={change}>{allTheme.textColor==="black"  ?<Night/>: <Day/>}</button>
         <select onChange={changeLan} name="language"  className='bg-blue-400  outline-none  text-white rounded-sm h-full pl-2 pr-2'>
           <option value="python">Python</option>
