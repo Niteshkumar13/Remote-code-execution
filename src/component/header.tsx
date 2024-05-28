044009e62126fc3276809af8aff1eb37de0f9bd3
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-28 22:00:36 +0530

    Update header.tsx

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,28 +1,15 @@
-import React, { FC, useState } from 'react'
-import { Verticle, Day, Night, Cross } from '../svg/svg';
+import React, { FC } from 'react'
+import { Code } from '../svg/svg';
 import {useTheme} from '../context/themeContext';
-import { usechangetheme } from '../hook/changeTheme';
 const Header:FC = () => {
-  const { allTheme, setAllTheme } = useTheme()
-  const {change} = usechangetheme({ allTheme, setAllTheme })
-  const [showSlider, setShowSlider] = useState(false)
+  const { allTheme} = useTheme()
   return (
     <div className='w-screen h-14 flex justify-center items-center' style={{ backgroundColor: allTheme.header }}>
       <div className='flex w-screen h-12 rounded-t-lg justify-start items-center'>
-        <label className='pl-2' onClick={e => setShowSlider(true)}><Verticle color={allTheme.textColor} /></label>
-        {showSlider && <div className='absolute top-0  w-[300px] h-screen border-r-2 z-10' style={{ backgroundColor: allTheme.slider,borderColor:allTheme.borderColor}}>
-          <div className='w-full flex justify-end pr-4 pt-4'><label onClick={e => setShowSlider(false)}><Cross color={allTheme.textColor} /></label></div>
-          <div className={`mt-4 w-full flex items-center h-11 border-t-2 border-b-2 justify-between pl-3 pr-3 hover:${allTheme.hoverColor}`} style={{borderColor:allTheme.borderColor}}>
-            <span className='text-xl' style={{color:allTheme.textColor}}>{allTheme.textColor==="black"  ?"Dark": "Light"}</span>
-            <button className=' rounded-full p-1' onClick={change}>{allTheme.textColor==="black"  ?<Night/>: <Day/>}</button>
-          </div>
-          <div className={`w-full flex items-center h-11 border-b-2 justify-between pl-3 pr-3 hover:${allTheme.hoverColor}`} style={{borderColor:allTheme.borderColor}}>
-            <a style={{color:allTheme.textColor}} href='/'>Help</a>
-          </div>
-        </div>}
+       <span className='ml-3'><Code color={allTheme.textColor}/></span>
       </div>
-      <div className='w-[98%] h-12  flex justify-end mr-2'>
-        <img alt='profile-pic' src='https://portfolio-fronted-two.vercel.app/static/media/WhatsApp_Image_2023-10-23_at_4.06.05_PM-removebg-preview.e8fb3b740caa42ee144f.png' className='max-h-12 min-w-min rounded-full' />
+      <div className='w-[98%] h-12  flex justify-end mr-4'>
+        <img alt='profile-pic' src='https://avatar.iran.liara.run/public/1' className='max-h-12 min-w-min rounded-full' />
       </div>
     </div>
   )
