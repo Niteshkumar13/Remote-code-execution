bfdeee4f10d46482fca0fb3f491fb9c44501dd64
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-28 21:59:44 +0530

    Update body.tsx

--- src/component/body.tsx
+++ src/component/body.tsx
@@ -2,26 +2,35 @@ import React from 'react';
 import { useTheme } from '../context/themeContext';
 import Editors from './editor';
 import {useLanguage}  from '../context/selectLanguage';
+import { Day, Night } from '../svg/svg';
+import { LuFiles } from "react-icons/lu";
+import { usechangetheme } from '../hook/changeTheme';
+import Slider from './slider';
 const Body = () => {
-  const { allTheme } = useTheme();
+  const { allTheme,setAllTheme  } = useTheme();
   const {lan,setLan}  = useLanguage()
+  const {change} = usechangetheme({ allTheme, setAllTheme })
   const changeLan = (e:any)=>{
     setLan({...lan,language:e.target.value})
   }
   return (
-    <div className=' h-[calc(100vh-3.5rem)] w-screen b'>
-      <div className='h-11 flex items-center pl-2 gap-1 border-t-2 border-b-2' style={{ backgroundColor: allTheme.body, borderColor: allTheme.borderColor }}>
-        <select onChange={changeLan} name="language"  className='bg-blue-400 outline-none text-white rounded-sm h-full pl-3 pr-3'>
+    <div className=' h-[calc(100vh-3.5rem)] w-screen '>
+      <div className='h-11 flex items-center pl-1 gap-[2px] border-t-2 border-b-2' style={{ backgroundColor: allTheme.body, borderColor: allTheme.borderColor }}>
+        <button className='bg-blue-400 h-full p-1 pl-2 pr-2 rounded'><LuFiles size={25}/></button>
+        <Slider/>
+        <button className='bg-blue-400 h-full pl-2 pr-2 rounded' onClick={change}>{allTheme.textColor==="black"  ?<Night/>: <Day/>}</button>
+        <select onChange={changeLan} name="language"  className='bg-blue-400  outline-none  text-white rounded-sm h-full pl-2 pr-2'>
           <option value="python">Python</option>
           <option value="javascript">JavaScript</option>
           <option value="c">C</option>
           <option value="cpp">C++</option>
+          <option value="html">HTML</option>
         </select>
         <button className='bg-blue-400  rounded-sm h-full pl-6 pr-6 text-lg text-white'>Run</button>
       </div>
-      <div className=' h-[calc(100vh-6.3rem)] w-screen flex'>
+      <div className='h-[calc(100vh-6.3rem)] w-screen flex flex-wrap'>
         <Editors />
-        <div className=' w-1/2  h-full max-sm:w-screen max-sm:h-1/2' style={{ backgroundColor: allTheme.body, color: allTheme.textColor }}></div>
+        <div className=' w-[30%]  h-full max-md:w-screen max-md:h-1/2 border-t-[1px]' style={{ backgroundColor: allTheme.body, color: allTheme.textColor,borderColor: allTheme.borderColor }}></div>
       </div>
     </div>
   )
