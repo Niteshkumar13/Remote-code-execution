13bd8bbdbfef50fea080df49f8c8973301ab2ad5
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-29 20:46:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import Header from './component/header';
 import Body from './component/body';
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <>
+    <Toaster 
+         position="top-right" 
+         toastOptions={{
+           duration: 1000,
+           style: {
+             background: 'white',
+             color: 'black',
+           },
+           success: {
+             duration: 1000,
+             theme: {
+               primary: 'green',
+               secondary: 'black',
+             },
+            },
+          }}
+             />
    <Header/>
    <Body/>
   </>
