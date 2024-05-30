55b574fde9a36afa59a88f77bfc244bb5d2276ff
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-05-30 06:57:03 +0530

    Update selectLanguage.tsx

--- src/context/selectLanguage.tsx
+++ src/context/selectLanguage.tsx
@@ -1,42 +1,55 @@
-import React,{FC, ReactNode, createContext,useContext,useState} from "react";
-import { language,languageState,startedSnippet } from "../interfaces/interface";
-const Lanaguage = createContext<languageState|null>(null);
-export const useLanguage = ()=> {
+import React, { FC, ReactNode, createContext, useContext, useState } from "react";
+import { language, languageState, startedSnippet } from "../interfaces/interface";
+const Lanaguage = createContext<languageState | null>(null);
+export const useLanguage = () => {
     const context = useContext(Lanaguage)
-    if(!context){
+    if (!context) {
         throw new Error("useLanguage must be used within a LanaguageProvider")
     }
     return context
 };
- const LanguageContext:FC<{children:ReactNode}> = ({children})=>{
-  const [lan,setLan] = useState<language>({
-  language:'python',
-  snippet:``
+const LanguageContext: FC<{ children: ReactNode }> = ({ children }) => {
+    const [lan, setLan] = useState<language>({
+        language: 'python',
+        snippet: ``
     })
-    return(
-        <Lanaguage.Provider value={{lan,setLan}}>
+    return (
+        <Lanaguage.Provider value={{ lan, setLan }}>
             {children}
         </Lanaguage.Provider>
     )
 }
 // started snippet
-export const def_code:startedSnippet = {
+export const def_code: startedSnippet = {
     c: `#include <stdio.h>
       int main() {
-         // printf() displays the string inside quotation
          printf("Hello, World!");
          return 0;
       }`,
     python: `# some example\nprint("Hello World")`,
     javascript: `//some code snippet\nconsole.log("Hello World");`,
-    cpp: 
-      `// Your First C++ Program
+    cpp:
+        `// Your First C++ Program
       #include <iostream>
       int main() {
           std::cout << "Hello World!";
           return 0;
       }
       `,
-  };
+    html: 
+`<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
+    `
+};
 
 export default LanguageContext;
